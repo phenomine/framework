399360b3add3def3a7a10e3777d1ee357ad3539a
Muhammad Fahli Saputra <saputra@fahli.net>
2023-09-27 18:39:35 +0700

    refactor: change namespace

--- src/Phenomine/Support/Console/Commands/Routes/RouteListCommand.php
+++ src/Phenomine/Support/Console/Commands/Routes/RouteListCommand.php
@@ -2,11 +2,11 @@
 
 namespace Phenomine\Support\Console\Commands\Routes;
 
-use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class RouteListCommand extends Command
+class RouteListCommand extends SymfonyCommand
 {
     protected $name = 'route:list ';
     protected $description = 'List all registered routes';
