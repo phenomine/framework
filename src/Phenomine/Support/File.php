c47d70e2bb6127bd09ef581eb34059634cba02af
Muhammad Fahli Saputra <saputra@fahli.net>
2023-11-10 03:08:19 +0700

    build: create make controller command

--- src/Phenomine/Support/File.php
+++ src/Phenomine/Support/File.php
@@ -67,6 +67,44 @@ class File
 
         return $file;
     }
+    public static function createFileFromString($origin, $string, $extension = '.php')
+    {
+        // check if last $string is .php
+        if (Str::endsWith($string, '.php')) {
+            $string = Str::removeLast($string, '.php');
+        }
+
+        $keys = Str::split($string, ['.', '/', '\\']);
+
+        $file_dir = $origin;
+        $filename = $string;
+        $directory = null;
+        // if $keys has more than 1 element, expect the last element as filename and the rest as directory
+        // create directory if not exists
+        if (count($keys) > 1) {
+            $filename = $keys[count($keys) - 1];
+            $directory = '';
+            for ($i = 0; $i < count($keys) - 1; $i++) {
+                $directory .= $keys[$i].'/';
+            }
+            $directory = rtrim($directory, '/');
+            if (!static::exists($origin.'/'.$directory)) {
+                mkdir($origin.'/'.$directory, 0777, true);
+            }
+            $file_dir = $origin.'/'.$directory;
+        }
+
+        $file = $file_dir.'/'.$filename.$extension;
+        if (!static::exists($file)) {
+            touch($file);
+            return [
+                'file' => $file,
+                'directory' => $directory
+            ];
+        } else {
+            return false;
+        }
+    }
 
     public static function getName($path)
     {
@@ -76,5 +114,24 @@ class File
     public static function getExtension($path)
     {
         return pathinfo($path, PATHINFO_EXTENSION);
+    }
+
+    public static function read($path)
+    {
+        return file_get_contents($path);
+    }
+
+    public static function readAndReplace($path, $params)
+    {
+        $content = file_get_contents($path);
+        foreach ($params as $key => $value) {
+            $content = str_replace('{{'.$key.'}}', $value, $content);
+        }
+        return $content;
+    }
+
+    public static function write($path, $content)
+    {
+        file_put_contents($path, $content);
     }
 }
--- src/Phenomine/Support/Str.php
+++ src/Phenomine/Support/Str.php
@@ -20,4 +20,174 @@ class Str
     {
         return strlen($string);
     }
+
+    public static function contains($string, $needle)
+    {
+        return strpos($string, $needle) !== false;
+    }
+
+    public static function startsWith($string, $needle)
+    {
+        return strpos($string, $needle) === 0;
+    }
+
+    public static function endsWith($string, $needle)
+    {
+        return substr($string, -strlen($needle)) === $needle;
+    }
+
+    public static function replace($string, $search, $replace)
+    {
+        return str_replace($search, $replace, $string);
+    }
+
+    public static function replaceFirst($string, $search, $replace)
+    {
+        return preg_replace('/'.$search.'/', $replace, $string, 1);
+    }
+
+    public static function replaceLast($string, $search, $replace)
+    {
+        $pos = strrpos($string, $search);
+        if ($pos !== false) {
+            $string = substr_replace($string, $replace, $pos, strlen($search));
+        }
+        return $string;
+    }
+
+    public static function split($string, $delimiter = '.')
+    {
+        if (is_array($delimiter)) {
+            $regex = '/[';
+            foreach ($delimiter as $key => $value) {
+                if ($value == '\\') {
+                    $regex .= "\\\\\\\\";
+                } else if ($value == '/') {
+                    $regex .= '\\/';
+                } else {
+                    $regex .= $value;
+                }
+            }
+            $regex .= ']/';
+            return preg_split($regex, $string);
+        } else {
+            return explode($delimiter, $string);
+        }
+    }
+
+    public static function trim($string, $character_mask = " \t\n\r\0\x0B")
+    {
+        return trim($string, $character_mask);
+    }
+
+    public static function trimLeft($string, $character_mask = " \t\n\r\0\x0B")
+    {
+        return ltrim($string, $character_mask);
+    }
+
+    public static function trimRight($string, $character_mask = " \t\n\r\0\x0B")
+    {
+        return rtrim($string, $character_mask);
+    }
+
+    public static function toLower($string)
+    {
+        return strtolower($string);
+    }
+
+    public static function toUpper($string)
+    {
+        return strtoupper($string);
+    }
+
+    public static function toTitle($string)
+    {
+        return ucwords($string);
+    }
+
+    public static function toCamel($string)
+    {
+        return lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $string))));
+    }
+
+    public static function toSnake($string)
+    {
+        return strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $string));
+    }
+
+    public static function toKebab($string)
+    {
+        return strtolower(preg_replace('/(?<!^)[A-Z]/', '-$0', $string));
+    }
+
+    public static function toSlug($string)
+    {
+        return strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $string), '-'));
+    }
+
+    public static function random($length = 16)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = Str::length($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+    public static function randomNumeric($length = 16)
+    {
+        $characters = '0123456789';
+        $charactersLength = Str::length($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+    public static function randomAlpha($length = 16)
+    {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = Str::length($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+    public static function randomAlphaNumeric($length = 16)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = Str::length($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+    public static function randomUuidV4()
+    {
+        return sprintf(
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            random_int(0, 65535),
+            random_int(0, 65535),
+            random_int(0, 65535),
+            random_int(16384, 20479),
+            random_int(32768, 49151),
+            random_int(0, 65535),
+            random_int(0, 65535),
+            random_int(0, 65535)
+        );
+    }
+
+    public static function removeLast($string, $needle)
+    {
+        return preg_replace('/'.$needle.'$/', '', $string);
+    }
+
+
 }
